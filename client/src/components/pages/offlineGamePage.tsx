fe9905d06091160f6e384ea7f637bdabddbcc98b
Amit Kahlon <50583120+Amitkahlon@users.noreply.github.com>
2023-04-28 12:50:23 +0300

    basic game with useState bug

--- client/src/components/pages/offlineGamePage.tsx
+++ client/src/components/pages/offlineGamePage.tsx
@@ -13,6 +13,10 @@ export enum team {
 
 export type entityType = 'pawn' | 'king';
 
+export enum printType {
+  types,
+  visibility,
+}
 class Board {
   public board: Cell[][];
 
@@ -29,6 +33,46 @@ class Board {
   public getCell(x: number, y: number) {
     return this.board[y][x];
   }
+
+  public getEntity(x: number, y: number) {
+    return this.board[y][x].entity;
+  }
+
+  public getClone() {
+    const clone = new Board();
+    clone.board = [...this.board];
+
+    return clone;
+  }
+
+  /**
+   * helper function
+   */
+  public printBoard(pt: printType = printType.types) {
+    let str = '';
+
+    if (pt === printType.types) {
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          str += this.board[i][j].isEmpty() ? '0'.padStart(4, ' ').padEnd(4, ' ') : this.board[i][j].entity?.type;
+          str += ', ';
+        }
+        str += '\n';
+      }
+    } else if (pt === printType.visibility) {
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          str += this.board[i][j].isEmpty()
+            ? '0'.padStart(4, ' ').padEnd(4, ' ')
+            : this.board[i][j].entity?.isVisible?.toString();
+          str += ', ';
+        }
+        str += '\n';
+      }
+    }
+
+    console.log(str);
+  }
 }
 
 class Cell {
@@ -37,10 +81,15 @@ class Cell {
   constructor(public x: number, public y: number) {
     this.entity = null;
   }
+
+  public isEmpty() {
+    return this.entity === null;
+  }
 }
 
 class MarkerBoard {
   public markerBoard: number[][];
+  public readonly HIGHLIGHT_INDICATOR = 1;
 
   constructor() {
     this.markerBoard = [[], [], [], [], [], [], [], []];
@@ -49,6 +98,29 @@ class MarkerBoard {
         this.markerBoard[y][x] = 0;
       }
     }
+  }
+
+  public setHighlight(x: number, y: number) {
+    this.markerBoard[y][x] = this.HIGHLIGHT_INDICATOR;
+  }
+
+  public isHighlighted(x: number, y: number) {
+    return this.markerBoard[y][x] === this.HIGHLIGHT_INDICATOR;
+  }
+
+  /**
+   * Returns true if the board has at least 1 highlight
+   */
+  public hasAnyHighlight() {
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        if (this.isHighlighted(x, y)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 }
 
@@ -61,9 +133,13 @@ export class Entity {
   };
   public isVisible: boolean;
 
+  constructor() {
+    this.isVisible = false;
+  }
+
   public getPossibleMoves?(x: number, y: number, board: Board): MarkerBoard;
 
-  protected isOutsideBorders(x: number, y: number) {
+  protected isInsideBorders(x: number, y: number) {
     return x >= 0 && x <= 7 && y >= 0 && y <= 7;
   }
 
@@ -83,10 +159,17 @@ export class Pawn extends Entity {
 
   public override getPossibleMoves(x: number, y: number, board: Board): MarkerBoard {
     const markerBoard = new MarkerBoard();
-    if (this.team === team.black && !this.isOutsideBorders(x, y + 1)) {
-      markerBoard.markerBoard[y + 1][x] = 1;
-    } else if (!this.isOutsideBorders(x, y - 1)) {
-      markerBoard.markerBoard[y - 1][x] = 1;
+    if (
+      this.team === team.black &&
+      this.isInsideBorders(x, y + 1) &&
+      (board.getCell(x, y + 1).isEmpty() || board.getEntity(x, y + 1)?.team !== this.team)
+    ) {
+      markerBoard.setHighlight(x, y + 1);
+    } else if (
+      this.isInsideBorders(x, y - 1) &&
+      (board.getCell(x, y - 1).isEmpty() || board.getEntity(x, y - 1)?.team !== this.team)
+    ) {
+      markerBoard.setHighlight(x, y - 1);
     }
 
     return markerBoard;
@@ -112,14 +195,7 @@ export class King extends Entity {
   }
 
   public override getPossibleMoves(x: number, y: number, board: Board): MarkerBoard {
-    const markerBoard = new MarkerBoard();
-    if (this.team === team.black && !this.isOutsideBorders(x, y + 1)) {
-      markerBoard.markerBoard[y + 1][x] = 1;
-    } else if (!this.isOutsideBorders(x, y - 1)) {
-      markerBoard.markerBoard[y - 1][x] = 1;
-    }
-
-    return markerBoard;
+    return new MarkerBoard();
   }
 
   public override effectOnEntity(entity: Entity, board: Board) {
@@ -136,9 +212,15 @@ type IPiecesSetup = {
 
 class Team {
   piecesSetup: IPiecesSetup;
+  isReady: boolean;
+  readonly FIRST_COLUMN;
+  readonly SECOND_COLUMN;
 
-  constructor(public team: team) {
-    this.piecesSetup = { king: 8, pawn: 0 };
+  constructor(public team: team, firstColumn: number, secondColumn: number) {
+    this.piecesSetup = { king: 2, pawn: 0 };
+    this.isReady = false;
+    this.FIRST_COLUMN = firstColumn;
+    this.SECOND_COLUMN = secondColumn;
   }
 }
 
@@ -152,20 +234,29 @@ class GameManager {
   public setupFinished: boolean;
   public blackTeam: Team;
   public whiteTeam: Team;
+  private _teamTurn: Team;
+  public get teamTurn(): Team {
+    return this._teamTurn;
+  }
+  public set teamTurn(value: Team) {
+    console.log(value.team);
+    this._teamTurn = value;
+  }
+  public turnCount: number;
 
-  public setPiece(entity: Entity, pos: Position) {
-    if (this.setupFinished) throw new Error('Setup is not finished');
+  public setup_setPiece(entity: Entity, pos: Position) {
+    if (this.setupFinished) throw new Error('Setup is finished');
 
     const { x, y } = pos;
     const t = entity.team;
     let currentTeam: Team;
-    let allowedYColumn: 7 | 0;
+    let allowedYColumn: number;
     if (t === team.black) {
       currentTeam = this.blackTeam;
-      allowedYColumn = 0;
+      allowedYColumn = this.blackTeam.FIRST_COLUMN;
     } else {
       currentTeam = this.whiteTeam;
-      allowedYColumn = 7;
+      allowedYColumn = this.whiteTeam.FIRST_COLUMN;
     }
 
     const leftSetupCount = currentTeam.piecesSetup[entity.type];
@@ -185,36 +276,111 @@ class GameManager {
     currentTeam.piecesSetup[entity.type] -= 1;
   }
 
+  public setPiece(entity: Entity, pos: Position) {
+    if (this.setupFinished) throw new Error('Setup is finished');
+    const { x, y } = pos;
+    const t = entity.team;
+    let currentTeam: Team;
+    if (t === team.black) {
+      currentTeam = this.blackTeam;
+    } else {
+      currentTeam = this.whiteTeam;
+    }
+
+    if (this.board.getCell(x, y).entity !== null) {
+      throw new Error('Cannot place pieces where a piece is already placed');
+    }
+
+    this.board.getCell(x, y).entity = entity;
+  }
+
   public move(entity: Entity, newPos: Position, currPos: Position) {
+    if (entity.team !== this.teamTurn.team) {
+      throw new Error('Its not the turn of team: ' + entity.team.toString());
+    }
+
     if (!this.setupFinished) {
       throw new Error('Setup is not finished');
     }
 
-    const isMovePossible = entity.getPossibleMoves?.(currPos.x, currPos.y, this.board).markerBoard[newPos.y][newPos.x];
+    const isMovePossible = entity
+      .getPossibleMoves?.(currPos.x, currPos.y, this.board)
+      .isHighlighted(newPos.x, newPos.y);
+
     if (!isMovePossible) {
+      console.error('new Pos: ', newPos);
+      console.error('curr Pos: ', currPos);
+      console.error('entity Pos: ', entity);
+
       throw new Error('Move is not possible');
     }
-
     if (this.board.getCell(newPos.x, newPos.y).entity !== null) {
-      entity.effectOnEntity?.(entity, this.board);
-      return;
+      entity.effectOnEntity?.(this.board.getCell(newPos.x, newPos.y).entity as Entity, this.board);
+    } else {
+      this.board.getCell(newPos.x, newPos.y).entity = entity;
+      this.board.getCell(currPos.x, currPos.y).entity = null;
     }
 
-    this.board.getCell(newPos.x, newPos.y).entity = entity;
+    this.passTurn();
   }
 
-  constructor() {
-    this.board = new Board();
-    this.setupFinished = false;
-    this.blackTeam = new Team(team.black);
-    this.whiteTeam = new Team(team.white);
+  public setReady(t: Team) {
+    t.isReady = true;
 
-    for (let i = 0; i < 8; i++) {
-      this.board.getCell(i, 6).entity = new Pawn(team.white);
+    if (this.blackTeam.isReady && this.whiteTeam.isReady) {
+      for (let i = 0; i < 8; i++) {
+        if (this.board.getCell(i, this.blackTeam.FIRST_COLUMN).entity == null) {
+          this.setPiece(new Pawn(team.black), { x: i, y: this.blackTeam.FIRST_COLUMN });
+        }
+      }
+
+      for (let i = 0; i < 8; i++) {
+        if (this.board.getCell(i, this.whiteTeam.FIRST_COLUMN).entity == null) {
+          this.setPiece(new Pawn(team.white), { x: i, y: this.whiteTeam.FIRST_COLUMN });
+        }
+      }
+
+      this.setupFinished = true;
     }
+  }
 
-    for (let i = 0; i < 8; i++) {
-      this.board.getCell(i, 1).entity = new Pawn(team.black);
+  public getOppositeTeam(oppositeTeam: Team) {
+    return oppositeTeam.team === team.white ? this.blackTeam : this.whiteTeam;
+  }
+
+  public getClone() {
+    const clone = new GameManager(false);
+
+    clone.blackTeam = this.blackTeam;
+    clone.whiteTeam = this.whiteTeam;
+    clone.board = this.board;
+    clone.setupFinished = this.setupFinished;
+    clone.turnCount = this.turnCount;
+    clone.teamTurn = this.teamTurn;
+    return clone;
+  }
+
+  public passTurn() {
+    this.teamTurn = this.getOppositeTeam(this.teamTurn);
+    this.turnCount++;
+  }
+
+  constructor(init: boolean = true) {
+    if (init) {
+      this.board = new Board();
+      this.setupFinished = false;
+      this.blackTeam = new Team(team.black, 0, 1);
+      this.whiteTeam = new Team(team.white, 7, 6);
+      this.turnCount = 0;
+      this.teamTurn = this.whiteTeam;
+
+      for (let i = 0; i < 8; i++) {
+        this.board.getCell(i, 6).entity = new Pawn(team.white);
+      }
+
+      for (let i = 0; i < 8; i++) {
+        this.board.getCell(i, 1).entity = new Pawn(team.black);
+      }
     }
   }
 }
@@ -229,34 +395,89 @@ function reducer(state: GameManager, action: { type: string; payload?: any }) {
   }
 }
 
+interface selectedEntity {
+  x: number;
+  y: number;
+  entity: Entity;
+}
+
 const OfflineGamePage = () => {
-  const [gameManager, dispatch] = useState(new GameManager());
+  const [gameManager, setGameManager] = useState(new GameManager());
   const [currentTeam, setCurrentTeam] = useState(gameManager.whiteTeam);
-  const [selectedEntity, setSelectedEntity] = useState<Entity | null>(null);
+  const [selectedEntity, setSelectedEntity] = useState<selectedEntity | null>(null);
   const [highlightBoard, setHighlightBoard] = useState<MarkerBoard>(new MarkerBoard());
 
   const renderPieceImage = (entity: Entity) => {
     if (entity == null) return null;
-    return <img width={60} height={30} src={entity.team === team.black ? entity.image.black : entity.image.white} />;
+    const image = entity.team === team.black ? entity.image.black : entity.image.white;
+
+    if (entity.team !== currentTeam.team && !entity.isVisible) {
+      return <p>?</p>;
+    }
+
+    return <img width={60} height={30} src={image} />;
   };
 
   const handleCellClick = (cell: Cell) => {
-    if (!gameManager.setupFinished && selectedEntity && !cell.entity) {
-      try {
-        gameManager.setPiece(selectedEntity, { x: cell.x, y: cell.y });
-        cell.entity = selectedEntity;
-        setSelectedEntity(null);
-        setHighlightBoard(new MarkerBoard());
-      } catch (error) {
-        console.error(error);
+    // handle setup clicks
+    if (!gameManager.setupFinished) {
+      if (selectedEntity && !cell.entity) {
+        try {
+          gameManager.setup_setPiece(selectedEntity.entity, { x: cell.x, y: cell.y });
+          cell.entity = selectedEntity.entity;
+          setSelectedEntity(null);
+          setHighlightBoard(new MarkerBoard());
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+    // handle game clicks
+    else if (gameManager.setupFinished) {
+      if (cell.entity?.team === currentTeam.team) {
+        setSelectedEntity({ entity: cell.entity, x: cell.x, y: cell.y });
+        const highlightBoard = cell.entity.getPossibleMoves?.(cell.x, cell.y, gameManager.board) as MarkerBoard;
+        highlightBoard.setHighlight(cell.x, cell.y);
+
+        setHighlightBoard(highlightBoard);
+      } else if (selectedEntity) {
+        try {
+          gameManager.move(
+            selectedEntity.entity,
+            { x: cell.x, y: cell.y },
+            { x: selectedEntity.x, y: selectedEntity.y },
+          );
+
+          setSelectedEntity(null);
+          const gmClone = gameManager.getClone();
+          setGameManager(gmClone);
+          setHighlightBoard(new MarkerBoard());
+
+          doRandomMove(gameManager.teamTurn);
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
   };
 
   const renderGameSetup = () => {
+    if (gameManager.setupFinished) {
+      return null;
+    }
+
     const currentTeamPiecesSetup = currentTeam.piecesSetup;
     const pawnImage = currentTeam.team === team.white ? whitePawnImage : blackPawnImage;
     const kingImage = currentTeam.team === team.white ? whiteKingImage : blackKingImage;
+
+    let isFinished: boolean = true;
+    for (const k in currentTeamPiecesSetup) {
+      let key = k as entityType;
+      if (currentTeamPiecesSetup[key] !== 0) {
+        isFinished = false;
+        break;
+      }
+    }
 
     return (
       <div>
@@ -270,11 +491,11 @@ const OfflineGamePage = () => {
                 return;
                 //show error
               }
-              setSelectedEntity(new King(currentTeam.team));
+              setSelectedEntity({ entity: new King(currentTeam.team), x: -1, y: -1 });
               const highlightBoard = new MarkerBoard();
               for (let i = 0; i < 8; i++) {
                 if (!gameManager.board.getCell(i, 7).entity) {
-                  highlightBoard.markerBoard[i][7] = 1;
+                  highlightBoard.setHighlight(i, 7);
                 }
               }
 
@@ -283,13 +504,73 @@ const OfflineGamePage = () => {
           />
           <p>Count: {currentTeamPiecesSetup.king}</p>
         </div>
-
-        <div>
-          <img src={pawnImage} width={100} height={100} />
-          <p>Count: {currentTeamPiecesSetup.pawn}</p>
-        </div>
+        {isFinished ? (
+          <div>
+            <button onClick={handleReadyClick}>Ready</button>
+          </div>
+        ) : null}
       </div>
     );
+  };
+
+  const handleReadyClick = () => {
+    gameManager.setReady(currentTeam);
+    const oppositeTeam = gameManager.getOppositeTeam(currentTeam);
+
+    let num1, num2;
+    do {
+      num1 = Math.floor(Math.random() * 8);
+      num2 = Math.floor(Math.random() * 8);
+    } while (num1 === num2);
+
+    gameManager.setup_setPiece(new King(oppositeTeam.team), { x: num1, y: oppositeTeam.FIRST_COLUMN });
+    gameManager.setup_setPiece(new King(oppositeTeam.team), { x: num2, y: oppositeTeam.FIRST_COLUMN });
+    gameManager.setReady(oppositeTeam);
+
+    setGameManager(gameManager.getClone()); //hack to update the ui..
+    setSelectedEntity(null);
+  };
+
+  /**
+   * MEGA CODE
+   * @param t
+   */
+  const doRandomMove = (t: Team) => {
+    const currentTeamOnBoardEntities: { entity: Entity; pos: { x: number; y: number } }[] = [];
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const currEntity = gameManager.board.getEntity(j, i);
+        if (
+          currEntity &&
+          currEntity.team === t.team &&
+          currEntity.getPossibleMoves?.(j, i, gameManager.board).hasAnyHighlight()
+        ) {
+          currentTeamOnBoardEntities.push({ entity: currEntity, pos: { x: j, y: i } });
+        }
+      }
+    }
+    const randomEntity = currentTeamOnBoardEntities[Math.floor(Math.random() * currentTeamOnBoardEntities.length)];
+    const possibleMovesBoard = randomEntity.entity.getPossibleMoves?.(
+      randomEntity.pos.x,
+      randomEntity.pos.y,
+      gameManager.board,
+    );
+
+    const possibleMovesArr: { x: number; y: number }[] = [];
+
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (possibleMovesBoard?.isHighlighted(j, i)) {
+          possibleMovesArr.push({ x: j, y: i });
+        }
+      }
+    }
+
+    const randomMove = possibleMovesArr[Math.floor(Math.random() * possibleMovesArr.length)];
+
+    gameManager.move(randomEntity.entity, randomMove, randomEntity.pos);
+
+    // gameManager.board.printBoard();
   };
 
   return (
@@ -298,12 +579,12 @@ const OfflineGamePage = () => {
       {!gameManager.setupFinished ? <p>Place your pieces</p> : <p>Game started</p>}
 
       <div className="board">
-        {gameManager.board.board?.map((row: Cell[], x: number) => (
-          <div key={x} className="row">
-            {row.map((cell, y) => (
+        {gameManager.board.board?.map((row: Cell[], y: number) => (
+          <div key={y} className="row">
+            {row.map((cell, x) => (
               <button
-                key={y}
-                className={classNames('cell', { selected: 1 === highlightBoard.markerBoard[y][x] })}
+                key={x}
+                className={classNames('cell', { selected: highlightBoard.isHighlighted(x, y) })}
                 onClick={() => handleCellClick(cell)}
               >
                 {renderPieceImage(cell?.entity as Entity)}
